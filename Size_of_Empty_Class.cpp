7dd1bcc43c1b5d7050b020e66fd45ebf4f2dbaab
AmanKas <aman312000@gmail.com>
2023-11-07 13:14:14 +0530

    Size of Empty Class

---
+++ Size_of_Empty_Class.cpp
@@ -0,0 +1,15 @@
+//Size of Empty Class
+#include<bits/stdc++.h>
+using namespace std;
+
+class Hospital {
+    //empty class
+};
+
+int main()
+{
+    Hospital Fortis;
+    cout << "Size of the Fortis Class: " << sizeof(Fortis);
+
+    return 0;
+}
